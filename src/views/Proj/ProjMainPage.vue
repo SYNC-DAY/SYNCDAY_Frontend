0b84a5bc94c8513e3a5994c71fcc45136a05ba11
1etterh <1etterh.dev@gmail.com>
2024-11-21 15:44:38 +0900

    feat: axios로 백엔드랑 통신

--- src/views/Proj/ProjMainPage.vue
+++ src/views/Proj/ProjMainPage.vue
@@ -41,18 +41,52 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
+import axios from 'axios';
+
 import SideBar from '@/components/layout/SideBar.vue';
 import ProjItem from './components/ProjItem.vue';
 import WorkspaceItem from './components/WorkSpaceItem.vue';
+
+import {useAuthStore} from "@/stores/auth.js";
+import {storeToRefs} from "pinia";
+
+const authStore = useAuthStore()
+const { user } = storeToRefs(authStore)
+
+
+const projs = ref([]);
+
 const activeWorkspace = ref(null)
 const activeProject = ref(null)
 const selectWorkspace = (workspace) => {
   activeWorkspace.value = workspace
 }
 
+
 const selectProject = (project) => {
   activeProject.value = project
 }
 
+
+/* onMounted */
+const fetchProjs = async(userId) => {
+
+  console.log("fetching projs")
+  try{
+    const response = await axios.get(`/proj-members/users/${userId}`);
+
+    if(response.data.success){
+      projs.value = response.data;
+    } else{
+      throw new Error(response.data.error || 'Failed to fetch Projs');
+    }
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+onMounted(()=>{
+  fetchProjs(user.value.userId);
+})
 </script>
