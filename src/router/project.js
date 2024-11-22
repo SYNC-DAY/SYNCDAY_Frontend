e32381193f65cc038872c65a0f981166d0de6ce1
1etterh <1etterh.dev@gmail.com>
2024-11-22 09:03:14 +0900

    chore: develop 풀 받기 전에 커밋

--- src/router/project.js
+++ src/router/project.js
@@ -4,13 +4,5 @@ export default [
         name: "Project",
         component: () => import('@/views/Proj/ProjMainPage.vue'),
         meta: { requiresAuth: true }
-    }, 
-
-    {
-        path: '/workspaces/:workspaceId',
-        name: 'Workspace',
-        component: () => import('@/views/Workspace/WorkspaceMainPage.vue'),
-        meta:{requiresAuth: true},
-        props: true
     }
 ]
