ea61ae3c5f6567321675edec33c7e633a62c5ca2
1etterh <1etterh.dev@gmail.com>
2024-11-25 17:31:30 +0900

    feat: vitedevtools 추가

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,7 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
-
+import vueDevTools from 'vite-plugin-vue-devtools';
 // https://vitejs.dev/config/
 export default defineConfig({
     optimizeDeps: {
@@ -14,7 +14,8 @@ export default defineConfig({
         vue(),
         Components({
             resolvers: [PrimeVueResolver()]
-        })
+        }),
+        vueDevTools()
     ],
     resolve: {
         alias: {
