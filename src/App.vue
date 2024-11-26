36cd76846a0b3199f578f67ccd8dd6dbd1be23b1
1etterh <1etterh.dev@gmail.com>
2024-11-26 15:09:24 +0900

    chore: 네비게이션 추가

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
-<script setup>
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
     <RouterView />
 </template>
 
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
 <style scoped></style>
