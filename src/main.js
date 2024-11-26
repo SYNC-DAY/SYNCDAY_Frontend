17747d7ffbcd1624cce82897ec7e7f5bf4f839da
1etterh <1etterh.dev@gmail.com>
2024-11-26 14:07:43 +0900

    chore: primary 색 변경

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { definePreset } from '@primevue/themes';
 import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
@@ -12,15 +13,30 @@ import '@/assets/tailwind.css';
 
 const app = createApp(App);
 
+/* router */
 app.use(router);
+
+/* prime vue */
+const SyncDayPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{pink.50}',
+            100: '{pink.100}',
+            200: '{pink.200}',
+            300: '{pink.300}',
+            400: '{pink.400}',
+            500: '{pink.500}',
+            600: '{pink.600}',
+            700: '{pink.700}',
+            800: '{pink.800}',
+            900: '{pink.900}',
+            950: '{pink.950}'
+        }
+    }
+});
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
-        options: {
-            prefix: 'p',
-            darkModeSelector: 'system',
-            cssLayer: false
-        }
+        preset: SyncDayPreset
     }
 });
 app.use(ToastService);
