bf37b557a331a8a4709923a11d950e1aed9d2926
dongkh9 <dongkh99@gamil.com>
2024-12-09 11:34:14 +0900

    feat(deploy): 배포 설정 추가
    github-actions 사용 배포 설정 추가

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const port = process.env.PORT || 8080;  
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
