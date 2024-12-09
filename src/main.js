ade5ccbf378acc4588a0511d59b1d523e89d7be6
1etterh <1etterh.dev@gmail.com>
2024-12-09 22:54:37 +0900

    feat: 기본적인 레이아웃 구성

--- src/main.js
+++ src/main.js
@@ -2,22 +2,42 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+import { definePreset } from '@primevue/themes';
 import Aura from '@primevue/themes/aura';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 import ToastService from 'primevue/toastservice';
 
 import '@/assets/styles.scss';
+import '@/assets/syncday/colors.css';
 import '@/assets/tailwind.css';
-
 const app = createApp(App);
 
 app.use(router);
+
+const SyncDayPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{purple.50}',
+            100: '{purple.100}',
+            200: '{purple.200}',
+            300: '{purple.300}',
+            400: '{purple.400}',
+            500: '{purple.500}',
+            600: '{purple.600}',
+            700: '{purple.700}',
+            800: '{purple.800}',
+            900: '{purple.900}',
+            950: '{purple.950}'
+        }
+    }
+});
 app.use(PrimeVue, {
     theme: {
-        preset: Aura,
+        preset: SyncDayPreset,
         options: {
-            darkModeSelector: '.app-dark'
+            prefix: 'p',
+            darkModeSelector: 'system'
         }
     }
 });
