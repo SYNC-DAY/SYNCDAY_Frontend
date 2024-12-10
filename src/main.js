2534ac7e12b64115d33d0099144cbd38809dac00
1etterh <1etterh.dev@gmail.com>
2024-12-10 23:09:06 +0900

    feat: primevue 속성 분리

--- src/main.js
+++ src/main.js
@@ -1,47 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import installPrimeVue from './plugins/primevue';
 import router from './router';
 
-import { definePreset } from '@primevue/themes';
-import Aura from '@primevue/themes/aura';
-import PrimeVue from 'primevue/config';
-import ConfirmationService from 'primevue/confirmationservice';
-import ToastService from 'primevue/toastservice';
-
+/* styles */
 import '@/assets/styles.scss';
 import '@/assets/syncday/main.css';
 import '@/assets/tailwind.css';
+
 const app = createApp(App);
 
 app.use(router);
-
-const SyncDayPreset = definePreset(Aura, {
-    semantic: {
-        primary: {
-            50: '{blue.50}',
-            100: '{blue.100}',
-            200: '{blue.200}',
-            300: '{blue.300}',
-            400: '{blue.400}',
-            500: '{blue.500}',
-            600: '{blue.600}',
-            700: '{blue.700}',
-            800: '{blue.800}',
-            900: '{blue.900}',
-            950: '{blue.950}'
-        }
-    }
-});
-app.use(PrimeVue, {
-    theme: {
-        preset: SyncDayPreset,
-        options: {
-            prefix: 'p',
-            darkModeSelector: '.app-dark'
-        }
-    }
-});
-app.use(ToastService);
-app.use(ConfirmationService);
+installPrimeVue(app);
 
 app.mount('#app');
---
+++ src/plugins/primevue.js
@@ -0,0 +1,37 @@
+import { definePreset } from '@primevue/themes';
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+const SyncDayPreset = definePreset(Aura, {
+    semantic: {
+        primary: {
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}'
+        }
+    }
+});
+
+export default function installPrimeVue(app) {
+    app.use(PrimeVue, {
+        theme: {
+            preset: SyncDayPreset,
+            options: {
+                prefix: 'p',
+                darkModeSelector: '.app-dark'
+            }
+        }
+    });
+    app.use(ToastService);
+    app.use(ConfirmationService);
+}
