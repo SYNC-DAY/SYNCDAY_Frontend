6dc579a55d5c5f08502cad699bbcb852b8e9a166
Vorschlag-bit <https://github.com/Vorschlag-bit/testR.git>
2024-12-10 15:23:25 +0900

    deploy

--- server.js
+++ server.js
@@ -5,14 +5,12 @@ import { dirname } from 'path';
 
 const app = express();
 const port = 8080;  
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
+app.use(express.static(__dirname));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
