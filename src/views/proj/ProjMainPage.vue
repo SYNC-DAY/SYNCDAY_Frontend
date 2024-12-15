ac06ef790664c9c30215a59d3b5309a70aa2d7c8
1etterh <1etterh.dev@gmail.com>
2024-12-15 19:59:36 +0900

    feat: Sidebar

--- src/views/proj/ProjMainPage.vue
+++ src/views/proj/ProjMainPage.vue
@@ -1,11 +1,22 @@
 <template>
     <div>프로젝트</div>
+    <div class="flex flex-row">
+        <Drawer v-model:visible="visibleLeft" position="left">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </Drawer>
+
+        <Button icon="pi pi-arrow-right" @click="visibleLeft = true" />
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+    import { ref } from 'vue';
+
+    const visibleLeft = ref(false);
+</script>
 
 <style scoped>
-div {
-    background-color: black;
-}
+    /* div {
+        background-color: black;
+    } */
 </style>
