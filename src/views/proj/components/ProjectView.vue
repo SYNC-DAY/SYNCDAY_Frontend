3cb0f14653d0af33f897a61b923bb77e2d55cd13
1etterh <1etterh.dev@gmail.com>
2024-12-18 15:23:55 +0900

    feat: 프로젝트 제목 표시

--- src/views/proj/components/ProjectView.vue
+++ src/views/proj/components/ProjectView.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-
-    </div>
+    <div class="flex flex-row justify-center"></div>
+    <span class="text-lg">{{ project?.proj_name }}</span>
 </template>
 
 <script setup>
+    import { useProjectStore } from '@/stores/proj/useProjectStore';
+    import { useWorkspaceStore } from '@/stores/proj/useWorkspaceStore';
+    import { onMounted, ref } from 'vue';
 
+    const projectStore = useProjectStore();
+    const workspaceStore = useWorkspaceStore();
+    const project = ref(null);
+
+    onMounted(() => {
+        // Get the active project from store
+        project.value = projectStore.projects[projectStore.activeId];
+    });
 </script>
 
 <style lang="scss" scoped></style>
