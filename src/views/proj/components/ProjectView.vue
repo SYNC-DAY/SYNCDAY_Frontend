47435c0595041a44adf96c725eae2a9072fe653b
1etterh <1etterh.dev@gmail.com>
2024-12-18 16:08:15 +0900

    feat:VcsTypeMenu

--- src/views/proj/components/ProjectView.vue
+++ src/views/proj/components/ProjectView.vue
@@ -6,10 +6,10 @@
         <h1 class="text-2xl text-center">{{ project?.proj_name }}</h1>
         <div class="flex flex-row items-center justify-between">
             <Button severity="contrast" text icon="pi pi-cog"></Button>
-            <Button severity="contrast" text>
+            <Button severity="contrast" text @click="toggleVcsMenu" aria-controls="overlay-menu">
                 <GitBranch class="h-4"></GitBranch>
             </Button>
-
+            <VcsTypeMenu ref="vcsMenu" @vcs-selected="handleVcsSelection" />
         </div>
     </div>
 </template>
@@ -18,9 +18,11 @@
     import { useProjectStore } from '@/stores/proj/useProjectStore';
     import { useWorkspaceStore } from '@/stores/proj/useWorkspaceStore';
     import { GitBranch } from 'lucide-vue-next';
-    import { computed } from 'vue';
+    import { computed, ref } from 'vue';
     import { useRoute } from 'vue-router';
+    import VcsTypeMenu from './utils/VcsTypeMenu.vue';
 
+    const vcsMenu = ref(null);
     const route = useRoute();
     const projectStore = useProjectStore();
     const workspaceStore = useWorkspaceStore();
@@ -28,6 +30,23 @@
     // Compute active project ID from route
     const activeProjectId = computed(() => route.params.projectId);
 
+    const toggleVcsMenu = event => {
+        vcsMenu.value?.toggle(event);
+    }
+    const handleVcsSelection = async vcsType => {
+        try {
+            if (vcsType === "GITHUB") {
+                showProjVcsSettings.value = true;
+            }
+        } catch (error) {
+            toast.add({
+                severity: "error",
+                summary: "VCS Connection Failed",
+                detail: error.message,
+                life: 3000,
+            });
+        }
+    };
     // Compute the active project
     const project = computed(() =>
         activeProjectId.value ? projectStore.projects[activeProjectId.value] : null
---
+++ src/views/proj/components/utils/VcsTypeMenu.vue
@@ -0,0 +1,26 @@
+<template>
+    <Menu ref="menuRef" :model="menuItems" :popup="true" />
+</template>
+
+<script setup>
+    import Menu from 'primevue/menu';
+    import { defineExpose, ref } from 'vue';
+
+    const menuRef = ref();
+    const menuItems = ref([{
+        label: "VCS Type",
+        items: [{
+            label: "GITHUB",
+            icon: "pi pi-github",
+            command: () => emit('vcs-selected', 'GITHUB')
+        }]
+    }]);
+
+    const toggle = (event) => {
+        menuRef.value?.toggle(event);
+    }
+
+    defineExpose({ toggle });
+
+    const emit = defineEmits(['vcs-selected']);
+</script>
